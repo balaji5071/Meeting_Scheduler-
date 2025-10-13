52cd8bc16c290c2259ef8315df6a7c99f302786f
Balaji <balajich0582gmail.com>
2025-10-13 16:23:44 +0530

    done

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://meeting-scheduler-backend-8au2.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
